7bab8f9b8612ecd2ef3d8b405ddd8da249da65b1
lenvaz <LWaz@rasckconsulting.com>
2025-10-22 19:32:46 +0530

    chore: add basepath to for the gh pages repository

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "",
+  // basePath: "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: "",
   images: {
     unoptimized: true,
